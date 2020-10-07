04a492795a8710ead32f32db60880f08ac2ad147
joncrall <erotemic@gmail.com>
2020-10-07 10:44:59 -0400

    wip

--- xdev/misc.py
+++ xdev/misc.py
@@ -508,3 +508,17 @@ def _print_forest(graph):
     for idx, node in enumerate(sources, start=1):
         islast_next = (idx == len(sources))
         _recurse(node, indent='', islast=islast_next)
+
+
+def make_warnings_print_tracebacks():
+    import warnings
+    import traceback
+    _orig_formatwarning = warnings.formatwarning
+    warnings._orig_formatwarning = _orig_formatwarning
+    def _monkeypatch_formatwarning_tb(*args, **kwargs):
+        s = _orig_formatwarning(*args, **kwargs)
+        if len(s.strip()):
+            tb = traceback.format_stack()
+            s += ''.join(tb[:-1])
+        return s
+    warnings.formatwarning = _monkeypatch_formatwarning_tb
