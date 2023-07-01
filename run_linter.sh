cbd18d8815edaec9fa8f1248120d9d14a6aa89d4
joncrall <erotemic@gmail.com>
2023-07-01 14:23:08 -0400

    wip

--- run_linter.sh
+++ run_linter.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-flake8 ./xdev --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics xdev
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics ./tests
