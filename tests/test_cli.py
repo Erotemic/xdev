c1c2c6f3f697953058f8c7afffe742d205e4dab5
joncrall <erotemic@gmail.com>
2023-08-18 11:33:33 -0400

    wip

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,8 +3,10 @@
 def test_xdev_cli():
     import ubelt as ub
     import xdev
+    import sys
+    py_exe = sys.executable
 
-    info = ub.cmd('python3 -m xdev --help', verbose=3)
+    info = ub.cmd(f'{py_exe} -m xdev --help', verbose=3)
     assert 'pint' in info['out'].strip()
     assert 'codeblock' in info['out'].strip()
     assert 'tree' in info['out'].strip()
@@ -14,14 +16,14 @@ def test_xdev_cli():
     except ImportError:
         ...
     else:
-        info = ub.cmd('python3 -m xdev pint 10gigabytes megabytes', verbose=3)
+        info = ub.cmd(f'{py_exe} -m xdev pint 10gigabytes megabytes', verbose=3)
         assert info['out'].strip() == '10000'
 
-    info = ub.cmd('python3 -m xdev info', verbose=3)
+    info = ub.cmd(f'{py_exe} -m xdev info', verbose=3)
     assert info['ret'] == 0
 
-    info = ub.cmd('python3 -m xdev info --help', verbose=3)
+    info = ub.cmd(f'{py_exe} -m xdev info --help', verbose=3)
     assert info['ret'] == 0
 
-    info = ub.cmd('python3 -m xdev pyversion xdev', verbose=3)
+    info = ub.cmd(f'{py_exe} -m xdev pyversion xdev', verbose=3)
     assert info['out'].strip() == xdev.__version__
